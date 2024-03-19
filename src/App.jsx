e17814052e26bf8bd866dbc05f37538a3482eaef
Aswin-Sta <aswin.001.official@gmail.com>
2024-03-19 10:18:35 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Flex, Text, Button } from "@radix-ui/themes";
+function App() {
+  return (
+    <>
+      <Flex direction="column" gap="2">
+        <Text>Hello from Radix Themes :</Text>
+        <Button>Lets go</Button>
+      </Flex>
+    </>
+  );
+}
+
+export default App;
